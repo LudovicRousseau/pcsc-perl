958befd4bc86ddbd3bc5886253a61e07e1d040b5
Ludovic Rousseau <ludovic.rousseau@free.fr>
2023-11-18 15:31:04 +0100

    create_distrib.sh: modernize and add missing double quotes

--- create_distrib.sh
+++ create_distrib.sh
@@ -8,30 +8,33 @@
 
 # exit in case of error
 set -e
+set -x
 
-dir=$(basename $(pwd))
+dir=$(basename "$(pwd)")
 
-echo -e "Using $dir as directory name\n"
+echo "Using $dir as directory name"
 
-rv=$(echo $dir | sed -e 's/.*-[0-9]\+\.[0-9]\+\.[0-9]\+/ok/')
-if [ $rv != "ok" ]
+SED=gsed
+rv=$(echo "$dir" | $SED -e 's/.*-[0-9]\+\.[0-9]\+\.[0-9]\+/ok/')
+if [ "$rv" != "ok" ]
 then
 	echo "ERROR: The directory name should be in the form foo-bar-x.y.z"
 	exit
 fi
 
-if [ -e $dir ]
+if [ -e "$dir" ]
 then
-	echo -e "ERROR: $dir already exists\nremove it and restart"
+	echo "ERROR: $dir already exists"
+	echo "remove it and restart"
 	exit
 fi
 
 # generate Changelog
-/usr/share/cvs/contrib/rcs2log > Changelog
+#/usr/share/cvs/contrib/rcs2log > Changelog
 
-present_files=$(tempfile)
-manifest_files=$(tempfile)
-diff_result=$(tempfile)
+present_files=$(mktemp)
+manifest_files=$(mktemp)
+diff_result=$(mktemp)
 
 # clean dir
 if [ -e Makefile ]
@@ -41,40 +44,41 @@ fi
 
 # find files present
 # remove ^debian and ^create_distrib.sh
-find -type f | grep -v CVS | cut -c 3- | grep -v ^create_distrib.sh | sort > $present_files
-cat MANIFEST | sort > $manifest_files
+find . -type f | grep -v CVS | cut -c 3- | grep -v ^create_distrib.sh | sort > "$present_files"
+cat MANIFEST | sort > "$manifest_files"
 
 # diff the two lists
-diff $present_files $manifest_files | grep '<' | cut -c 2- > $diff_result
+diff "$present_files" "$manifest_files" | grep '<' | cut -c 2- > "$diff_result"
 
-if [ -s $diff_result ]
+if [ -s "$diff_result" ]
 then
-	echo -e "WARNING! some files will not be included in the archive.\nAdd them in MANIFEST"
-	cat $diff_result
+	echo "ARNING! some files will not be included in the archive."
+	echo "Add them in MANIFEST"
+	cat "$diff_result"
 	echo
 fi
 
 # remove temporary files
-rm $present_files $manifest_files $diff_result
+rm "$present_files" "$manifest_files" "$diff_result"
 
 # create the temporary directory
-mkdir $dir
+mkdir "$dir"
 
 for i in $(cat MANIFEST)
 do
 	if [ $(echo $i | grep /) ]
 	then
 		idir=$dir/${i%/*}
-		if [ ! -d $idir ]
+		if [ ! -d "$idir" ]
 		then
 			echo "mkdir -p $idir"
-			mkdir -p $idir
+			mkdir -p "$idir"
 		fi
 	fi
 	echo "cp $i $dir/$i"
-	cp -a $i $dir/$i
+	cp -a "$i" "$dir/$i"
 done
 
-tar cjvf ../$dir.tar.bz2 $dir
-rm -r $dir
+tar cjvf ../"$dir".tar.bz2 "$dir"
+rm -r "$dir"
 
